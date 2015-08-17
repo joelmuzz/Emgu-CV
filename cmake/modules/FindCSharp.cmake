72e2d3449fda7a82e4eba02760a40606d1e8f09c
Canming Huang <support@emgu.com>
2015-08-17 14:39:23 -0400

    find mono compiler on OSX.

--- cmake/modules/FindCSharp.cmake
+++ cmake/modules/FindCSharp.cmake
@@ -27,6 +27,11 @@ $ENV{windir}/Microsoft.NET/Framework64/v3.5/
 $ENV{windir}/Microsoft.NET/Framework/v3.5/
 "C:/Windows/Microsoft.NET/Framework/v3.5"
 )
+#MESSAGE( "$ENV{programfiles}/Reference Assemblies/Microsoft/Framework/.NETFramework/v3.5/Profile/Client/" )
+FIND_FILE (CSC_MSCORLIB_35 mscorlib.dll
+"$ENV{programfiles}/Reference Assemblies/Microsoft/Framework/.NETFramework/v3.5/Profile/Client/"
+"C:/Program Files (x86)/Reference Assemblies/Microsoft/Framework/.NETFramework/v3.5/Profile/Client/"
+)
 IF(CSC_EXECUTABLE_35)
 SET (CSC_EXECUTABLE ${CSC_EXECUTABLE_35})
 ENDIF()
@@ -53,7 +58,7 @@ SET (CSC_EXECUTABLE ${CSC_EXECUTABLE_40})
 ENDIF()
 
 ELSE(WIN32)
-FIND_PROGRAM (CSC_EXECUTABLE gmcs)
+FIND_PROGRAM (CSC_EXECUTABLE mcs)
 ENDIF(WIN32)
 
 FIND_PROGRAM (GACUTIL_EXECUTABLE gacutil 
@@ -61,7 +66,8 @@ FIND_PROGRAM (GACUTIL_EXECUTABLE gacutil
 "$ENV{programfiles}/Microsoft SDKs/Windows/v6.0A/Bin"
 "C:/Program Files/Microsoft SDKs/Windows/v6.0/bin" 
 "C:/Program Files/Microsoft SDKs/Windows/v6.0A/bin" 
-/usr/lib/mono/2.0)
+/usr/lib/mono/2.0
+/usr/bin)
 
 FIND_PROGRAM (AL_EXECUTABLE al
 "C:/Program Files/Microsoft SDKs/Windows/v7.0/bin"
@@ -76,7 +82,8 @@ FIND_PROGRAM (AL_EXECUTABLE al
 "$ENV{programfiles}/Microsoft SDKs/Windows/v6.0A/Bin"
 $ENV{windir}/Microsoft.NET/Framework/v3.5
 $ENV{windir}/Microsoft.NET/Framework/v2.0.50727
-/usr/lib/mono/2.0)
+/usr/lib/mono/2.0
+/usr/bin)
 
 FIND_PROGRAM (RESGEN_EXECUTABLE resgen
 "C:/Program Files/Microsoft SDKs/Windows/v7.0/bin"
@@ -89,7 +96,7 @@ FIND_PROGRAM (RESGEN_EXECUTABLE resgen
 "$ENV{programfiles}/Microsoft SDKs/Windows/v6.0/Bin" 
 "$ENV{programfiles}/Microsoft SDKs/Windows/v6.0A/Bin"
 "$ENV{programfiles}/Microsoft Visual Studio 8/SDK/v2.0/Bin"
-/usr/bin/resgen)
+/usr/bin)
 
 # ----------------------------------------------------------------------------
 #  FIND MSBUILD
